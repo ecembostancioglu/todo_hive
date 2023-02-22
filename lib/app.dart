002cbae65b61092cbac7f5ec4ae4bbb6eba654f9
ecembostancioglu <ecembostancioglu21@gmail.com>
2023-02-22 17:17:01 +0300

    Added hive package

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:todo_hive/views/home.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: HomePage(),
+    );
   }
 }
