5795a9de7d9f8b7d41bd3da84ab6f6710fc80bac
ecembostancioglu <ecembostancioglu21@gmail.com>
2023-02-24 20:23:55 +0300

    Created Todo class

---
+++ lib/model/todo.dart
@@ -0,0 +1,12 @@
+class Todo{
+  final String id;
+  final String name;
+  final DateTime createdAt;
+  final bool isCompleted;
+
+  Todo({
+    required this.id,
+    required this.name,
+    required this.createdAt,
+    required this.isCompleted});
+}
